ed974a993940f02e6898f50870a3c6bc9773e571
oqf <quintas.oscar@gmail.com>
2021-02-11 16:43:51 +0100

    oqf-test

--- test/aws/terraform/ec2_role_share_rule/public_and_private_ec2_same_role/main.tf
+++ test/aws/terraform/ec2_role_share_rule/public_and_private_ec2_same_role/main.tf
@@ -11,7 +11,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.48.0"
 
-  name = "Ec2RoleShareRule1"
+  name            = "Ec2RoleShareRule1"
   azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
@@ -19,17 +19,17 @@ module "vpc" {
   enable_nat_gateway = true
   enable_vpn_gateway = true
 
-  cidr = "10.0.0.0/16"
-  manage_default_security_group= true
+  cidr                          = "10.0.0.0/16"
+  manage_default_security_group = true
   default_security_group_ingress = [
-          {
+    {
       from_port   = 22
       to_port     = 22
       protocol    = "tcp"
       description = "ssh"
       cidr_blocks = "0.0.0.0/0"
-    }]
-  default_security_group_egress =[]
+  }]
+  default_security_group_egress = []
 }
 
 data "aws_ami" "ubuntu" {
@@ -69,15 +69,41 @@ EOF
 
 }
 
+resource "aws_iam_role" "test_role2" {
+  name = "test_role2"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+}
+
 resource "aws_iam_instance_profile" "test_profile" {
   name = "test_profile"
-  role = "${aws_iam_role.test_role.name}"
+  role = aws_iam_role.test_role.name
+}
+
+resource "aws_iam_instance_profile" "test_profile2" {
+  name = "test_profile2"
+  role = aws_iam_role.test_role2.name
 }
 
 
 resource "aws_iam_role_policy" "test_policy" {
   name = "test_policy"
-  role = "${aws_iam_role.test_role.id}"
+  role = aws_iam_role.test_role.id
 
   policy = <<EOF
 {
@@ -97,16 +123,16 @@ EOF
 
 
 resource "aws_instance" "pub_ins" {
-  ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"
-  subnet_id = module.vpc.public_subnets[0]
+  ami                  = data.aws_ami.ubuntu.id
+  instance_type        = "t2.micro"
+  subnet_id            = module.vpc.public_subnets[0]
   iam_instance_profile = aws_iam_instance_profile.test_profile.name
 
 }
 
 resource "aws_instance" "priv_ins" {
-  ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"
-  subnet_id = module.vpc.private_subnets[0]
-  iam_instance_profile = aws_iam_instance_profile.test_profile.name
+  ami                  = data.aws_ami.ubuntu.id
+  instance_type        = "t2.micro"
+  subnet_id            = module.vpc.private_subnets[0]
+  iam_instance_profile = aws_iam_instance_profile.test_profile2.name
 }
